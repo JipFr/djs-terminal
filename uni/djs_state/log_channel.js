2c21842b380f0f5f90aabf71f5d0d17a03e568ca
Jip <jipfrijlink@gmail.com>
2019-12-05 21:15:53 +0100

    End of day

--- uni/djs_state/log_channel.js
+++ uni/djs_state/log_channel.js
@@ -16,7 +16,7 @@ module.exports = () => {
 
 		console.clear();
 		console.log(`\nSelected channel: ${`#${state.channel.name.bold.yellow}`.bold.yellow} in ${state.guild.name.bold.yellow}\n`);
-		state.channel.fetchMessages({ limit: 20 }).then(msgs => {
+		state.channel.fetchMessages({ limit: 100 }).then(msgs => {
 			msgs.sort((a, b) => a.createdAt - b.createdAt).forEach(msg => { console.log(get_message_log(msg)) });
 			console.log("—".repeat(30).bold.green);
 		});
